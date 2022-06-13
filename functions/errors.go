46b1eb8b548ca91c8e6219c72afba38a2dd7c5de
klarysz <klarysz@viantinc.com>
2022-06-13 21:04:47 +0200

    patched errors

--- functions/errors.go
+++ functions/errors.go
@@ -9,38 +9,38 @@ func (e Errors) RegisterError(message string) (string, error) {
 	return "", fmt.Errorf(message)
 }
 
-func (e Errors) AssertFloat(value interface{}) (string, error) {
+func (e Errors) AssertFloat(value interface{}) (bool, error) {
 	_, ok := value.(float64)
 	if !ok {
-		return "", fmt.Errorf("expected to got float but got %T", value)
+		return false, fmt.Errorf("expected to got float but got %T", value)
 	}
 
-	return "", nil
+	return true, nil
 }
 
-func (e Errors) AssertInt(value interface{}) (string, error) {
+func (e Errors) AssertInt(value interface{}) (bool, error) {
 	_, ok := value.(int)
 	if !ok {
-		return "", fmt.Errorf("expected to got int but got %T", value)
+		return false, fmt.Errorf("expected to got int but got %T", value)
 	}
 
-	return "", nil
+	return true, nil
 }
 
-func (e Errors) AssertString(value interface{}) (string, error) {
+func (e Errors) AssertString(value interface{}) (bool, error) {
 	_, ok := value.(string)
 	if !ok {
-		return "", fmt.Errorf("expected to got string but got %T", value)
+		return false, fmt.Errorf("expected to got string but got %T", value)
 	}
 
-	return "", nil
+	return true, nil
 }
 
-func (e Errors) AssertBool(value interface{}) (string, error) {
+func (e Errors) AssertBool(value interface{}) (bool, error) {
 	_, ok := value.(bool)
 	if !ok {
-		return "", fmt.Errorf("expected to got bool but got %T", value)
+		return false, fmt.Errorf("expected to got bool but got %T", value)
 	}
 
-	return "", nil
+	return true, nil
 }
